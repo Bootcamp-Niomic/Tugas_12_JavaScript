1c2a27816a147263b30d14c301ff7011624e5dcd
Havid <great_havid@yahoo.co.id>
2019-11-13 11:40:31 +0700

    Tugas 12 JS

---
+++ tugas12.js
@@ -0,0 +1,10 @@
+// ====== Tugas 12 JavaScript =====
+
+var data = ['Pisang','Jeruk','Apel','Mangga'];
+console.log(data);
+
+var data2 = data.pop();
+console.log(data)
+
+var data3 = data.shift();
+console.log(data)
